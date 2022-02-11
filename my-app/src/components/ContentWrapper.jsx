ce76ea050236e636ac22a1ae4418de24aeeb3af2
SolBena <sol.benavides95@gmail.com>
2022-02-11 20:37:40 -0300

    Subo Trabajo Practico

---
+++ my-app/src/components/ContentWrapper.jsx
@@ -0,0 +1,13 @@
+import TopBar from './TopBar';
+import ContentRowTop from './ContentRowTop';
+import Footer from './Footer';
+
+export default function ContentWrapper() {
+    return (
+        <div>
+            <TopBar />
+            <ContentRowTop />
+            <Footer />
+        </div>
+    )
+}
